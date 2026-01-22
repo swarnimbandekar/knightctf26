491dc72514120bd93f1ab55b3fb5c828dda449b0
swarnimbandekar <swarnim.24bcb7157@vitapstudent.ac.in>
2026-01-22 22:13:59 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ⚠️ repo name
+  base: '/knightctf26/', // ⚠️ repo name
   plugins: [react()],
 })
